d69f7adec03b83da644fdb64ff2721e7e4b975c7
rik <dk84864@gmail.com>
2018-09-22 21:03:28 +0530

    slideshow on home page removed

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -60,23 +60,6 @@ var animateHTML = function() {
 };
 animateHTML().init();
 
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    console.log(slides);
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
-
 function openImg(imgs) {
   // Get the expanded image
   var expandImg = document.getElementById("expanded-img");
