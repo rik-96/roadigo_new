22761c2d660f6c0d219938668ce1597e9771e1c5
rik <dk84864@gmail.com>
2018-09-24 21:39:25 +0530

    testing new wallpaper

--- public/javascripts/navbar-anim.js
+++ public/javascripts/navbar-anim.js
@@ -1,6 +1,6 @@
 $(document).scroll(function() {
   var y = $(this).scrollTop();
-  if (y > 50) {
+  if (y > 20) {
     $('#navbar').fadeIn();
     $('#navbar').removeClass('responsive');
     $('#navbar-at-landing').addClass('invis');
