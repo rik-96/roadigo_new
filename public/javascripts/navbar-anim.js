347ad3a1d30fd4146e5ba29e90a7b176531a2f0c
rik <dk84864@gmail.com>
2018-09-27 19:40:13 +0530

    rectifying navbar bug

--- public/javascripts/navbar-anim.js
+++ public/javascripts/navbar-anim.js
@@ -1,6 +1,6 @@
 $(document).scroll(function() {
   var y = $(this).scrollTop();
-  if (y > 15) {
+  if (y > 80) {
     $('#navbar').fadeIn();
     $('#navbar').removeClass('responsive');
     $('#navbar-at-landing').addClass('invis');
